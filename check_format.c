2d0ba0def1d310253c97099f2ef0133697227566
Maurice Haro <maurice.nzai@gmail.com>
2022-08-13 04:30:56 -0700

    Updated check_format.c

--- check_format.c
+++ check_format.c
@@ -18,7 +18,23 @@ print_t p[] = {
 {"s", print_s},
 {"i", print_i},
 {"d", print_d},
+{"b", print_b},
 
+{"u", print_u},
+
+{"o", print_o},
+
+{"x", print_x},
+
+{"X", print_X},
+
+{"p", print_p},
+
+{"S", print_S},
+
+{"r", print_r},
+
+{"R", print_R},
 
 
 {NULL, NULL}
